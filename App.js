a23525e32dca5d8caf0e13c2a9a253bbbe84e892
Praj78 <86727228+Praj78@users.noreply.github.com>
2021-11-30 23:10:30 +0530

    almost all changed

--- App.js
+++ App.js
@@ -2,16 +2,20 @@ import "./styles.css";
 import React,{useState} from "react"
 const list={
   polymath:[
-    {name: "Naval Ravikanth",subjects:"Skin In The Game,Thinking Fast and Slow"},
-    {name:"Balaji Srinivasan",subjects:"The Great CEO Within,AI Superpowers"}
+    {name: "Naval Ravikanth",subjects:"The Rational Optimist (4/5) ,Thinking Fast and Slow (4.5/5) "},
+   {name:"Marc Andreessen",subjects:"The Hard Thing About Hard Things (5/5) ,Behave: The Biology of Humans at Our Best and Worst(4/5) "},
+    {name:"Balaji Srinivasan",subjects:"The Great CEO Within (4/5) ,AI Superpowers (4.5/5) "}
   ],
   tech:[
-    {name:"Tanay Pratap",subjects:"Eloquent JavaScript,You Don't Know JS"},
-    {name:"Crypto",subjects:"A History of Money,The Basics of Bitcoins and Blockchains"}
+    {name:"Tanay Pratap",subjects:"Eloquent JavaScript (4/5) ,You Don't Know JS (4.5) "},
+    {name:"Vitalik Buterin",subjects:"Defi and Future of Finance (3.5/5) ,Blockchain simply Explained(4.5) "},
+     {name: "Elon Musk",subjects:" Structures: Or Why Things Don’t Fall Down (4.5/5) ,Superintelligence: Paths, Dangers, Strategies (5/5) "}
+    
   ],
   writing:[
-    {name:"David Perell",subjects:"nobody wants to read your  shit,rework"},
-    {name:"Seth Godin",subjects:"Originals,the art of possiblity"}
+    {name:"David Perell",subjects:"Nobody wants to read your  shit (4/5) ,Rework (5/5) "},
+    {name:"Seth Godin",subjects:"Originals (4/5) ,The art of possiblity (4.5/5) "},
+    {name:"Nassim Taleb",subjects:"The Mind Doesn't Work That Way (5/5) ,The Blank Swan: The End of Probability (4/5) "}
   ]
 }
 
@@ -25,7 +29,7 @@ function toshow(grat){
  return (
    <div className="App">
    <h1> 📚 book recommendation</h1>
-  <p style={{fontSize:"smaller"}}>{" "}select the genres to see the book recommendation of great people{" "}</p>
+  <p style={{fontSize:"smaller"}}>{" "}Select the genres to see the book recommendations of great people{" "}</p>
    <div>{
      Object.keys(list).map((grat)=>(
        <button
